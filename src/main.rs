b67acdbc04da943c3e4082c209b51e644a2379e9
Bence Fábián <begnoc@gmail.com>
2021-01-08 18:45:09 +0100

    factor out ppm code

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,18 @@
 mod color;
 mod p3;
+mod ppm;
 mod ray;
 mod v3;
 
 use color::Color;
 use p3::Point3;
+use ppm::Ppm;
 use ray::Ray;
 use v3::Vec3;
 
-struct Rect {
+pub struct Rect {
     width: usize,
     height: usize,
-}
-
-struct Ppm {
-    size: Rect,
-}
-
-impl Ppm {
-    pub fn new(size: Rect) -> Self {
-        Ppm { size }
-    }
-
-    pub fn write<W, F>(self: &Self, writer: &mut W, get_color: F) -> std::io::Result<()>
-    where
-        W: std::io::Write,
-        F: Fn(Rect) -> Color,
-    {
-        writer.write_all(b"P3\n")?;
-        writeln!(writer, "{} {}", self.size.width, self.size.height)?;
-        writer.write_all(b"255\n")?;
-        for height in (0..self.size.width).rev() {
-            for width in 0..self.size.width {
-                let color = get_color(Rect { width, height });
-                color.write(writer)?;
-            }
-        }
-        writer.flush()
-    }
 }
 
 fn ray_color(r: Ray) -> Color {
---
+++ src/ppm.rs
@@ -0,0 +1,29 @@
+use crate::Rect;
+use crate::color::Color;
+
+pub struct Ppm {
+    size: Rect,
+}
+
+impl Ppm {
+    pub fn new(size: Rect) -> Self {
+        Ppm { size }
+    }
+
+    pub fn write<W, F>(self: &Self, writer: &mut W, get_color: F) -> std::io::Result<()>
+    where
+        W: std::io::Write,
+        F: Fn(Rect) -> Color,
+    {
+        writer.write_all(b"P3\n")?;
+        writeln!(writer, "{} {}", self.size.width, self.size.height)?;
+        writer.write_all(b"255\n")?;
+        for height in (0..self.size.width).rev() {
+            for width in 0..self.size.width {
+                let color = get_color(Rect { width, height });
+                color.write(writer)?;
+            }
+        }
+        writer.flush()
+    }
+}
