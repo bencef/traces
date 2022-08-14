2e14ec1b6abf2df6c93c70d522257ae83e248c2d
Bence Fabian <bence.fabian@codecool.com>
2022-08-14 14:57:08 +0200

    store facing of normals

--- in_a_weekend/src/hittable/mod.rs
+++ in_a_weekend/src/hittable/mod.rs
@@ -6,6 +6,25 @@ pub struct HitRecord {
     point: Point3,
     normal: Vec3,
     scale: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new(ray: &Ray, normal: Vec3, scale: f64) -> Self {
+        let point = ray.at(scale);
+        let front_face = Vec3::dot(ray.dir(), ray.dir()) < 0.0;
+        let normal = if front_face {
+            normal
+        } else {
+            normal.scale(-1.0)
+        };
+        Self {
+            point,
+            normal,
+            scale,
+            front_face,
+        }
+    }
 }
 
 pub trait Hittable {
--- in_a_weekend/src/hittable/sphere.rs
+++ in_a_weekend/src/hittable/sphere.rs
@@ -7,11 +7,11 @@ pub struct Sphere {
 }
 
 impl Hittable for Sphere {
-    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
-        let oc: Vec3 = r.origin() - self.center;
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc: Vec3 = ray.origin() - self.center;
         // variables for quadratic equation
-        let a = Vec3::dot(r.dir(), r.dir());
-        let half_b = Vec3::dot(oc, r.dir());
+        let a = Vec3::dot(ray.dir(), ray.dir());
+        let half_b = Vec3::dot(oc, ray.dir());
         let c = Vec3::dot(oc, oc) - self.radius * self.radius;
         let discriminant = half_b * half_b - a * c;
 
@@ -33,15 +33,10 @@ impl Hittable for Sphere {
             return None;
         }
 
-        let point = r.at(root);
-        let scale = root;
-        let normal = (point - self.center).scale(1.0 / self.radius);
+        let normal = (ray.at(root) - self.center).scale(1.0 / self.radius);
 
-        let rec = HitRecord {
-            point,
-            normal,
-            scale,
-        };
+        let rec = HitRecord::new(ray, normal, root);
+
         Some(rec)
     }
 }
